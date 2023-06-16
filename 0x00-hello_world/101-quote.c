2ca3e27b9f4f56a1f9ce387ed2f5a6c5955f69de
Roza Alene <rozaalene@gmail.com>
2023-06-15 17:31:37 -0700

    print by write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- print "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
