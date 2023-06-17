be007f3230a305fd95044d1d1b68ef8b7811f6e7
Roza Alene <rozaalene@gmail.com>
2023-06-16 19:49:01 -0700

    print 2 digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 	 * for loop check the first digit number from 0 to 8
 	 * for loop the 2nd check the second digit from first digit + 1 to 9
 	 * print first and second diigit
-	 * if first digit less than 8 print , spece 
+	 * if first digit less than 8 print , spaces
 	 */
 	for (n = 0 ; n < 9 ; n++)
 	{
-		for (m = n+1 ; m <=9 ; m++)
+		putchar(n + '0');
+		for (m = n + 1 ; m <= 9 ; m++)
 		{
-			putchar(n + '0');
 			putchar(m + '0');
 		}
 		if (n < 8)
