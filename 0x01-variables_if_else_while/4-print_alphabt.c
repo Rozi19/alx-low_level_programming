9ffb3a57fad269094bf2a15fad3e3a778ddee44c
Roza Alene <rozaalene@gmail.com>
2023-06-16 17:14:48 -0700

    except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,12 @@ int main(void)
 
 	/* for and putchar to print the lower case */
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
 		/* if to check the character is not q or e */
-		if (ch != 'q' || 'e' )
+		if (ch != 'e')
+		{
+			if (ch != 'q')
 			putchar(ch);
+		}
 		printf("\n");
-	}
 	return (0);
 }
