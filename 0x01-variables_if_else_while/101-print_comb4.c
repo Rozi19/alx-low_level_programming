cff36067a5f77fb097100928330f1224012dc515
Roza Alene <rozaalene@gmail.com>
2023-06-17 05:11:41 -0700

    print three digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,28 +3,29 @@
  * main- print "all possible different combinations of three digits
  * Return: always zero
  */
-int main (void)
+int main(void)
 {
 	int x, y, z;
 
-	/* for loop use to x start from 0 to 7
+	/**
+	 *for loop use to x start from 0 to 7
 	 * the 2nd for loop use for y start from x+1 to 8
 	 * the 3rd for loop use for z start from y=1 to 9
 	 * then putchar print our three digits number
 	 */
        for (x = 0 ; x <= 7 ; x++)
-       for (y = x + 1 ; y <= 8 ; y++)
-	       for (z = y + 1 ; z <= 9 ; z++)
-	       {
-	putchar(x + '0');
-	putchar(y + '0');
-	putchar(z + '0');
-	if (x < 7)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	       }
-putchar('\n');
-	return (0);
+	       for (y = x + 1 ; y <= 8 ; y++)
+		       for (z = y + 1 ; z <= 9 ; z++)
+		       {
+			       putchar(x + '0');
+			       putchar(y + '0');
+			       putchar(z + '0');
+			       if (x < 7)
+			       {
+				       putchar(',');
+				       putchar(' ');
+			       }
+		       }
+       putchar('\n');
+       return (0);
 }
