0b6aa603231c7ed04e8f3e186e27326f9720eab5
Roza Alene <rozaalene@gmail.com>
2023-06-16 17:53:36 -0700

    print number by putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@ int main(void)
 {
         int n;
 
-/* for loop n to start from 0  and incrment unitle 9 */
-        for (n = 0 ; n < 10 ; n++)
-                /* we convert n into char by + 0 into putchar and print n */
-        putchar(n + '0');
-        putchar('\n');
-        return (0);
+	/* for loop n to start from 0  and incrment unitle 9 */
+        for (n = 0 ; n < 10; n++)
+		/* we convert n into char by + 0 into putchar and print n */
+		putchar(n + '0');
+	putchar('\n');
+	return (0);
 }
