520afba4c05f91dac3796844251cf36d07598a88
Roza Alene <rozaalene@gmail.com>
2023-06-19 02:55:34 -0700

    print two two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,16 +16,18 @@ int main(void)
 	for(x = 0 ; x <= 9 ; x++)
 		for(y = 0 ; y <= 8 ; y++)
 		{
-			putchar(x + '0');
-			putchar(y + '0');
-			putchar(' ');
 			for(n = 0 ; n <= 9 ; n++)
+			{
 				for(m = y+1 ; m <= 9 ; m++)
 				{
+					putcahr(x + '0');
+					putchar(y + '0');
+					putchar(' ');
 					putchar(n + '0');
 					putchar(m + '0');
+					putchar(',');
 				}
-			putchar(',');
+			}
 		}
 	putchar('\n');
 	return (0);
