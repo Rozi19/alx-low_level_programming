657c7a169c7bac7727ef194ecd56eb12aca60846
Roza Alene <rozaalene@gmail.com>
2023-06-20 13:06:08 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,20 +15,23 @@ void jack_bauer(void)
 	 */
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (x = 0; x <= 5; x++)
+			if ((i <= 1 && j <= 9;) || (i <= 2 && j <= 3))
 			{
-				for (y = 0; y <= 9; y++) 
+				for (x = 0; x <= 5; x++)
 				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(x + '0');
-					_putchar(y + '0');
-					_putchar('\n');
+					for (y = 0; y <= 9; y++) 
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(x + '0');
+						_putchar(y + '0');
+						_putchar('\n');
+					}
 				}
-			}
+			}	
 		}
 	}
 }
