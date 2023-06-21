f622c4babf2767bb20bd671b9b8b470d2da9f2e1
Roza Alene <rozaalene@gmail.com>
2023-06-21 05:32:31 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,54 +12,17 @@ void print_to_98(int n)
 	 * if n greater than 98
 	 * for i from n to 98
 	 */
-	if (n <= 0)
+	for (i = n; i <= 98; i++)
 	{
-		if (n < 98)
+		if (i <= 9)
+			_putchar(' ');
+		if (i <= 99 && i >= 10)
+			_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		if (i < 98)
 		{
-			for (i = n; i <= 98; i++)
-			{
-				if (i <= 98)
-					_putchar(' ');
-				if (i <= 9)
-					_putchar(' ');
-				if (i >= 100)
-				{
-					_putchar((i / 100) + '0');
-					_putchar((i / 10) + '0');
-				}
-				else if (i <= 99 && i >= 10)
-				{
-					_putchar((i / 10) + '0');
-				}
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-		else if (n > 98)
-		{
-			for (i = n; i >= 98; i--)
-			{
-				if (i <= 99)
-					_putchar(' ');
-				if (i >= 100)
-				{
-					_putchar((i / 100) + '0');
-					_putchar((i /10) + '0');
-				}
-				else if (i <= 99 && i >= 10)
-				{
-					_putchar((i / 10) + '0');
-				}
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-				}
-			}
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 }
