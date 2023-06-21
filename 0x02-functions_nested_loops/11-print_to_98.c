efd470b18965839b602bd8c2f6493737dfc4d0c1
Roza Alene <rozaalene@gmail.com>
2023-06-21 05:58:58 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,5 +26,23 @@ void print_to_98(int n)
 			}
 		}
 	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i <= 99)
+				_putchar(' ');
+			if (i >= 100)
+			{
+				_putchar((i / 100) + '0');
+				_putchar((i / 10) + '0');
+			}
+			else if (i <= 99)
+			{
+				_putchar((i / 10) + '0')
+			}
+			_putchar((i % 10) + '0');
+		}
+	}
 	_putchar('\n');
 }
