06817bee75fe1d789cba0d87403eb7f083ca5b60
Roza Alene <rozaalene@gmail.com>
2023-06-21 16:31:21 -0700

    fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Return: always zero
+ */
+int main(void)
+{
+	int c;
+	long int f = 1, s = 2;
+	long int n = f + s;
+
+	printf("%lu, ", f);
+	printf("%lu, ", s);
+	/**
+	 * for c strat from 3 to 50
+	 * if c equl to 50 print space
+	 * else if comma and c
+	 */
+	for (c = 3; c <= 50; c++)
+	{
+		if (c == 50)
+		{
+			printf("%lu \n", n);
+		}
+		else
+		{
+			printf("%lu, ", n)
+		}	
+		f = s;
+		s = n;
+		n = f + s;
+	}
+	return (0);
+}	
