e069d6d0c7b11fa50eae5f79e3d97d8ed132f105
Roza Alene <rozaalene@gmail.com>
2023-06-21 05:03:45 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,17 @@ void print_to_98(int n)
 	 * if n greater than 98
 	 * for i from n to 98
 	 */
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n; i <= 98; n++)
 		{
 			if (i <= 98)
 				_putchar(' ');
 			if (i <= 9)
+				_putchar(' ');
+			if (i >= -9)
+				_putchar(' ');
+			if (i >= -99)
 				_putchar(' ');
 			if (i >= 100)
 			{
