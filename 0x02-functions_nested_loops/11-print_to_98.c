adf7a98b97ede4ae36dbf3e633d2ba47a516ca8d
Roza Alene <rozaalene@gmail.com>
2023-06-21 03:37:51 -0700

    print natural-number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: pass the value
+ * Return: the value
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	/**
+	 * if n greater than 98
+	 * for i from n to 98
+	 */
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; n++)
+		{
+			if (i <= 98)
+				_putchar(' ');
+			if (i <= 9)
+				_putchar(' ');
+			if (i >= 100)
+			{
+				_putchar((i / 100) + '0');
+				_putchar((i / 10) + '0');
+			}
+			else if (i <= 99 && i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i <= 99)
+				_putchar(' ');
+			if (i >= 100)
+			{
+				_putchar((i / 100) + '0');
+				_putchar((i /10) + '0');
+			}
+			else if (i <= 99 && i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+			}
+		}
+	}
+}
+
