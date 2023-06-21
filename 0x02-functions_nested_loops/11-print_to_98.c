13a5d78780d0b93f85362836646359ff7b53f926
Roza Alene <rozaalene@gmail.com>
2023-06-21 05:18:47 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		if (n < 98)
 		{
-			for (i = n; i <= 98; n++)
+			for (i = n; i <= 98; i++)
 			{
 				if (i <= 98)
 					_putchar(' ');
@@ -63,4 +63,3 @@ void print_to_98(int n)
 		}
 	}
 }
-
