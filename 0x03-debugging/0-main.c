9085b0f83ed124de4c054123ffbc8e963430a771
Roza Alene <rozaalene@gmail.com>
2023-06-22 01:35:17 -0700

    testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - test a number is postitive or negative
+ * Return: always zero
+ */
+int main(void)
+{
+	int n;
+
+	n = 98;
+	postitive_or_negative(n);
+	return (0);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+0. Multiple mains:- you’re just testing it!
+		You only need to upload 0-main.c and main.h for this task. We will provide our own positive_or_negative() function
