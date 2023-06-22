9587888adaa5c74cdd8482b43376b951ad8ec30f
Roza Alene <rozaalene@gmail.com>
2023-06-22 12:11:44 -0700

    Squre

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,15 @@ void print_square(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
