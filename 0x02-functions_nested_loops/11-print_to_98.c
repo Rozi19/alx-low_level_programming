8d6f54024547c484911974e358c5717572b22845
Roza Alene <rozaalene@gmail.com>
2023-06-21 19:32:01 -0700

    98 natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,20 +24,37 @@ void print_to_98(int n)
 	}
 	else if (n < 0)
 	{
-		if (n < 0)
-			_putchar('-');
 		for (i = n; i <= 98; i++)
 		{
-			if (i <= -100)
+			if (i < = 0)
 			{
-				_putchar((i / 100) + '0');
-				_putchar((i % 100 / 10) + '0');
+				_putchar('-');
+				if (i <= -100)
+				{
+					_putchar(-(i / 100) + '0');
+					_putchar(-(i % 100 / 10) + '0');
+				}
+				else if (i >= -99 && i <= -10)
+				{
+					_putchar(-(i / 10) + '0');
+				}
+				_putchar(-(i % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
-			else if (i >= 99 && i <= 10)
+			else if (i >= 0)
 			{
-				_putchar((i / 10) + '0');
+				if (i < 99 && i >= 10)
+				{
+					_putchar((i / 10) + '0');
+				}
+				_putchar((i % 10) + '0');
+				if (i < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			_putchar((i % 10) + '0');
 		}
 	}
 	else if (n > 98)
